8a425bf8dcc609489255713cbfad09a77c3922aa
skoripal <156136115+skoripal@users.noreply.github.com>
2024-05-17 00:33:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,35 +4,33 @@ import requests
 
 st.title('Predict Charges')
 
-# Load sidebar options from a JSON file
+
 with open('input_options.json') as f:
     side_bar_options = json.load(f)
     options = {}
     for key, value in side_bar_options.items():
         min_val, max_val = value
-        current_value = (min_val + max_val) // 2
+        current_value = ( min_val + max_val ) // 2
         options[key] = st.sidebar.slider(key, min_val, max_val, value=current_value)
 
-# Display the current options selected by the user
+
 st.write(options)
 
-# When the user clicks the 'Predict' button
-if st.button('Predict'):
-    # Prepare the payload for the POST request
+if st.button('Predict'): 
+    # payload = json.dumps({'inputs': options})
+    # response = requests.post(
+    #     url=f"http://159.203.68.179:5001/invocations",
+    #     data=payload,
+    #     headers={"Content-Type": "application/json"},
+    # )
+
     payload = json.dumps(options)
-    try:
-        # Make a POST request to the API
-        response = requests.post(
-            url="http://45.55.203.144:3333/docs/",
-            data=payload,
-            headers={"Content-Type": "application/json"},
-        )
-        response_data = response.json()
-        print(response_data)
-        if 'prediction' in response_data:
-            prediction = response_data['prediction']
-            st.write(f'The predicted Policy Charges is: ${prediction:,}')
-        else:
-            st.error('No prediction found in the response.')
-    except Exception as e:
-        st.error(f"Failed to retrieve prediction. Error: {e}")
+    response = requests.post(
+        url=f"http://45.55.203.144:3333/docs/",
+        data=payload,
+        headers={"Content-Type": "application/json"},
+    )
+
+    response = response.json()
+    prediction = response.get('prediction')
+    st.write(f'The predicted Policy Charges is: ${prediction:,}')
